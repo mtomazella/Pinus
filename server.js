b2c67bedf9fb812f1c9c4dd5a5b761e5f60181d1
MatheusTomazella <matheustomazella250504@gmail.com>
2020-11-22 12:49:11 -0300

    2020-11-22 - Deploy Debug 2

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const http      = require( 'http' ).Server( app );
 const io        = require( 'socket.io' )( http );
 const config    = require( './config.json' );
 
-http.listen( process.env.PORT || config.serverPort, ( error ) => {
+http.listen( process.env.PORT || 3305, ( error ) => {
     if ( error ) throw error;
     console.log( 'API Started' );
 } );
